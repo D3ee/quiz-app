30ebed75aba4e0e5d5aed466215f99286f608638
Dee <549033740@qq.com>
2026-04-23 13:10:16 +0800

    fix: 修复首页分类卡片布局为4列

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -247,7 +247,7 @@ function startFav() {
 /* Cards Grid */
 .categories {
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   gap: 24px;
   margin-bottom: 48px;
 }
